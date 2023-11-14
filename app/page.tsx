1b8a17f9e8ff74d585098cef5a05dde25628a2d1
AndonMitev <mitevandon94@gmail.com>
2023-11-14 12:36:09 +0200

    releaseDraft: false

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Image from 'next/image';
 import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      // const { checkUpdate } = await import('@tauri-apps/api/updater');
-      // const update = await checkUpdate();
-      // console.log('update', update);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { checkUpdate } = await import('@tauri-apps/api/updater');
+
+  //     const update = await checkUpdate();
+
+  //     console.log('update', update);
+  //   })();
+  // }, []);
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
