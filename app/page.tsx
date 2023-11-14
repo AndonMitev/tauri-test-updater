61c106ceb66711a039840cb7baa5eaf947359516
AndonMitev <mitevandon94@gmail.com>
2023-11-14 13:16:53 +0200

    feat: add update logic

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,26 @@ import { useEffect } from 'react';
 export default function Home() {
   useEffect(() => {
     (async () => {
-      const { checkUpdate } = await import('@tauri-apps/api/updater');
+      const { relaunch } = await import('@tauri-apps/api/process');
+      const { checkUpdate, installUpdate } = await import(
+        '@tauri-apps/api/updater'
+      );
 
-      const update = await checkUpdate();
+      const shouldUpdate = await checkUpdate();
 
-      console.log('update', update);
+      if (shouldUpdate) {
+        // You could show a dialog asking the user if they want to install the update here.
+        // console.log(
+        //   `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
+        // )
+
+        // Install the update. This will also restart the app on Windows!
+        await installUpdate();
+
+        // On macOS and Linux you will need to restart the app manually.
+        // You could use this step to display another confirmation dialog.
+        await relaunch();
+      }
     })();
   }, []);
 
